b5fde0ea332bae9afc85cf79191050a4cac1ccab
srinivasrao452 <30620328+srinivasrao452@users.noreply.github.com>
2019-05-18 17:41:34 +0530

    addfile

---
+++ Dog_Pro/Dog_App/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+
+    image = models.ImageField(upload_to='Images/', default='Images/None/No-img.jpg')
+    doc = models.FileField(upload_to='Doc/' , default='Doc/None/No-docs.PDF')
